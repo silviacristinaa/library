4d7ec0cc98e263d6b27edc8877dd8ffacc8922a4
silviacristinaa <silviacristinaalexandre1@gmail.com>
2025-03-27 09:25:49 -0300

    Primeiro commit

---
+++ src/test/java/com/github/silviacristinaa/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.silviacristinaa.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
