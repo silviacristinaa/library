ea0f745f264c1902a6a4618aaa27b885a588e16c
silviacristinaa <silviacristinaalexandre1@gmail.com>
2025-04-17 17:10:27 -0300

    [feature/03] - Configuração OpenApi

--- src/main/java/com/github/silviacristinaa/library/resources/BookResource.java
+++ src/main/java/com/github/silviacristinaa/library/resources/BookResource.java
@@ -5,8 +5,11 @@ import com.github.silviacristinaa.library.dtos.requests.BookStatusRequestDto;
 import com.github.silviacristinaa.library.dtos.responses.BookResponseDto;
 import com.github.silviacristinaa.library.exceptions.NotFoundException;
 import com.github.silviacristinaa.library.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -19,24 +22,28 @@ import java.net.URI;
 @RestController
 @RequestMapping(value = "/books")
 @RequiredArgsConstructor
+@Tag(name = "Books", description = "Book management service")
 public class BookResource {
 
     private static final String ID = "/{id}";
 
     private final BookService bookService;
 
+    @Operation(summary = "Get all")
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
-    public ResponseEntity<Page<BookResponseDto>> findAll(Pageable pageable) {
+    public ResponseEntity<Page<BookResponseDto>> findAll(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(bookService.findAll(pageable));
     }
 
+    @Operation(summary = "Get by id")
     @GetMapping(value = ID)
     @ResponseStatus(value = HttpStatus.OK)
     public ResponseEntity<BookResponseDto> findById(@PathVariable Long id) throws NotFoundException {
         return ResponseEntity.ok(bookService.findOneBookById(id));
     }
 
+    @Operation(summary = "Create")
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public ResponseEntity<Void> create(@RequestBody @Valid BookRequestDto bookRequestDto) {
@@ -45,6 +52,7 @@ public class BookResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Patch status")
     @PatchMapping(value = ID)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> updateBookStatus(
@@ -53,6 +61,7 @@ public class BookResource {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Update")
     @PutMapping(value = ID)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> update(@PathVariable Long id,
@@ -61,6 +70,7 @@ public class BookResource {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete")
     @DeleteMapping(value = ID)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> delete(@PathVariable Long id) throws NotFoundException {
